d98b008fd2ef8f104978696a1e0862b83a00af7b
Sobczakus1658 <103337067+Sobczakus1658@users.noreply.github.com>
2023-03-26 00:35:00 +0100

    optymalizacja

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -1,16 +1,15 @@
 global inverse_permutation
 ;rdi - rozmiar tablicy
 ;rsi - wskaznik na tablice 
-; na poczatku sprawdzic czy n dodatnie
 ;INT_MAX jeszcze może byc ->poprawić 
-; połączyc loop z correct
 inverse_permutation:
-    mov  rbx, 0x0
+    xor  rbx, rbx
+    mov r9, 0x7fffffff
     test rdi, rdi
     js   exit
     je   exit
-    cmp  rdi, 0x7fffffff
-    jns  exit
+    cmp  r9, rdi
+    js  exit
 loop:
     ; patrzę czy rbx mniejsze od n, jezeli tak to idz do correct
     mov  eax, [rsi+4*rbx]
@@ -22,10 +21,10 @@ loop:
     inc  QWORD rbx
     cmp  rdi, rbx
     jne  loop
-    mov  rbx, 0x0
+    xor  rbx, rbx
 correct:
     mov  eax, [rsi + 4*rbx]
-    and  rax, 0x7fffffff
+    and  rax, r9
     mov  ecx, [rsi + 4*rax]
     test ecx, ecx
     js   repair 
@@ -34,25 +33,21 @@ correct:
     inc  QWORD rbx 
     cmp  rdi, rbx
     jne  correct
-    mov  rbx, 0x0
+    xor  rbx, rbx
 permutation:
     mov  eax, [rsi + 4*rbx]
     test eax, eax
     jns  after_inverse
-    ;and rax, 0x7fffffff 
-    ;mov  [rsi + 4*rbx],eax
-    ;x    -ecx
     ;edx  - aktualny indeks
     ;r8d  -pozycja
     mov  edx, ebx
     mov  r8d, ebx
 inverse:
-    and  eax, 0x7fffffff 
+    and  eax, r9d
     mov  [rsi+4*rdx], r8d
     mov  r8d, edx
-    mov  ecx, [rsi+4*rax]
     mov  edx, eax
-    mov  eax, ecx
+    mov  eax, [rsi + 4*rax]
     test eax, eax
     js   inverse
     mov  [rsi + 4*rdx], r8d
@@ -63,17 +58,17 @@ after_inverse:
     mov  eax, 0x1
     ret
 exit:
-    mov  eax, 0x0
+    xor  eax, eax
     ret
 repair:
-    mov  rbx, 0x0
+    xor  rbx, rbx
 repair_loop:
     mov  eax, [rsi+4*rbx]
-    and  eax, 0x7fffffff
+    and  eax, r9d
     mov  [rsi+4*rbx], eax 
     inc  QWORD rbx
     cmp  rdi, rbx
     jne  repair_loop
-    mov  eax, 0x0
+    xor  eax, eax
     ret
     
